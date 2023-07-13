2f7346f23e192104b42f82498361ff2c57b6909e
Frso <ferasmalmsteen@gmail.com>
2023-07-13 11:59:57 +0200

    not complete

--- script_01.js
+++ script_01.js
@@ -22,3 +22,29 @@
 // let familyName;
 // familyName = "Mütze";
 // console.log(familyName);
+
+// let firstName, familyName;
+
+// // firstName = "Max";
+// firstName = prompt("Bitte Vornamen eingeben");
+// familyName = prompt("Bitte Nachnamen eingeben");
+
+// console.log(firstName + " " + familyName);
+
+// let test;
+// test = "Maxine";
+// test = 2;
+// test = false;
+
+// console.log(test);
+
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+
+ageJohn = 2023 - birthYearJohn;
+ageMark = 2023 - birthYearMark;
+
+
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
